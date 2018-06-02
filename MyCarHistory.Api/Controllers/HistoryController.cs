ec75ce784a23c073298f1575be04740279ffc723
Küzdi Tamás <kuzditomi@gmail.com>
2018-06-02 23:31:58 +0200

    add api

---
+++ MyCarHistory.Api/Controllers/HistoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCarHistory.Api.Controllers
+{
+    [Route("api/history")]
+    public class HistoryController : Controller
+    {
+        public HistoryController()
+        {
+        }
+
+        [Route("")]
+        [HttpGet]
+        public IActionResult GetHistory()
+        {
+            var history = new int[] { 1, 2, 3 };
+            return Ok(history);
+        }
+    }
+}
